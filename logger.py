dd5c0cfd3d0cc8529522f0f466c44236052d4aa6
Ariel Vardy <77558607+ArJonVar@users.noreply.github.com>
2022-12-19 19:22:53 -0800

    Add files via upload

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ from datetime import datetime
 class ghetto_logger:
     def __init__(self, location):
         raw_now = datetime.now()
-        self.now = raw_now.strftime("%d/%m/%Y %H:%M:%S")
+        self.now = raw_now.strftime("%m/%d/%Y %H:%M:%S")
         self.location=location
         self.first_use=True
         self.signature = f"\n{self.now} {location}   "
@@ -12,22 +12,22 @@ class ghetto_logger:
 
     def stamped_new_line(self, text, mode="a"):
         try:
-            with open(self.server_log_file, mode=mode) as file:
+            with open(self.dev_log_file, mode=mode) as file:
                 file.write(self.signature + text)
         except:
-            with open(self.server_log_file, mode=mode) as file:
+            with open(self.dev_log_file, mode=mode) as file:
                 file.write(self.signature + text)
 
     def new_line(self, text, mode="a"):
         try:
-            with open(self.server_log_file, mode=mode) as file:
+            with open(self.dev_log_file, mode=mode) as file:
                 if self.first_use == True:
                     file.write(self.signature + text)
                     self.first_use = False
                 elif self.first_use == False:
                     file.write("\n  " + text)
         except:
-            with open(self.server_log_file, mode=mode) as file:
+            with open(self.dev_log_file, mode=mode) as file:
                 if self.first_use == True:
                     file.write(self.signature + text)
                     self.first_use = False
@@ -36,14 +36,14 @@ class ghetto_logger:
 
     def paragraph(self, text, mode="a"):
         try:
-            with open(self.server_log_file, mode=mode) as file:
+            with open(self.dev_log_file, mode=mode) as file:
                 if self.first_use == True:
                     file.write(self.signature + text)
                     self.first_use = False
                 elif self.first_use == False:
                     file.write(text)
         except:
-            with open(self.server_log_file, mode=mode) as file:
+            with open(self.dev_log_file, mode=mode) as file:
                 if self.first_use == True:
                     file.write(self.signature + text)
                     self.first_use = False
